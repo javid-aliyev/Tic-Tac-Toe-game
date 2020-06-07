ea7b7da81bae80f58a503baedd96d4f5701b2a2d
Javid Aliyev <lorderer12@gmail.com>
2020-06-07 19:00:11 +0400

    rewrite check_winner function

--- game.py
+++ game.py
@@ -20,16 +20,14 @@ def print_board(b):
 	print("+---+---+---+")
 
 def check_winner(b):
-	if b[0] == b[4] == b[8] or \
-		b[2] == b[4] == b[6] or \
-		b[0] == b[1] == b[2] or \
-		b[3] == b[4] == b[5] or \
-		b[6] == b[7] == b[8] or \
-		b[0] == b[3] == b[6] or \
-		b[1] == b[4] == b[7] or \
-		b[2] == b[5] == b[8]:
-		return True
-	return False
+	return b[0] == b[4] == b[8] or \
+			b[2] == b[4] == b[6] or \
+			b[0] == b[1] == b[2] or \
+			b[3] == b[4] == b[5] or \
+			b[6] == b[7] == b[8] or \
+			b[0] == b[3] == b[6] or \
+			b[1] == b[4] == b[7] or \
+			b[2] == b[5] == b[8]
 
 def format_board(b):
 	if b[0] == b[4] == b[8]:
